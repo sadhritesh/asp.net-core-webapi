48d461fe49e89670a9d4a9bdaf494043e93f6240
Ritesh Sadh <sadhritesh32@gmail.com>
2025-03-06 15:36:28 +0530

    first asp.net core web api project

---
+++ WebApplication1/Controllers/FruitsAPIController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsAPIController : ControllerBase
+    {
+
+        public List<string> fruits = new List<string>()
+            {
+                "Banana",
+                "Kiwi",
+                "Dragon-fruit",
+                "papaya",
+                "grapes"
+
+            };
+
+        [HttpGet]
+         public List<string> GetFruits()
+        {
+            return fruits;
+        }
+
+        [HttpGet("{id}")]
+        public string GetFruitsByIndex(int id)
+        {
+            return fruits.ElementAt(id);
+        }
+
+    }
+}
