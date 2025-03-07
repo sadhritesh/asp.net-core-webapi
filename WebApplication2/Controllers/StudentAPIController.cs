d1292e2534b75649dc8bab95e070ef880266d28a
Ritesh Sadh <sadhritesh32@gmail.com>
2025-03-07 12:03:45 +0530

    WebApplication2

---
+++ WebApplication2/Controllers/StudentAPIController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly MydbContext context;
+
+        public StudentAPIController(MydbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudents()
+        {
+            var data = await context.Students.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentById(int id)
+        {
+            Student student = await context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student student)
+        {
+            await context.Students.AddAsync(student);
+            await context.SaveChangesAsync();
+
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<ActionResult<Student>> UpdateStudent(Student student, int id)
+        {
+            if (student.Id != id)
+            {
+                return BadRequest();
+            }
+            context.Entry(student).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+
+            return Ok(student);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(int id)
+        {
+            Student student =  await context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+            context.Students.Remove(student);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication2/Models/MydbContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models;
+
+public partial class MydbContext : DbContext
+{
+    public MydbContext()
+    {
+    }
+
+    public MydbContext(DbContextOptions<MydbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+        }
+
+    }
+
+       
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__students__3214EC07311813DC");
+
+            entity.ToTable("students");
+
+            entity.Property(e => e.FatherName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Standard)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
